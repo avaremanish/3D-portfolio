fa3977d28fe757ba82292a4121db12e4f7b54b3c
Manish Kumar <95077592+avaremanish@users.noreply.github.com>
2024-01-17 14:59:37 +0530

    get started

---
+++ script.js
@@ -0,0 +1,31 @@
+
+gsap.to("#page1",{
+    scrollTrigger:{
+        trigger: '#page1',
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        pin: true,
+        scroller:'#main'
+    }
+})
+gsap.to("#page2",{
+    scrollTrigger:{
+        trigger: '#page2',
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        pin: true,
+        scroller:'#main'
+    }
+})
+gsap.to("#page3",{
+    scrollTrigger:{
+        trigger: '#page3',
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        pin: true,
+        scroller:'#main'
+    }
+})
